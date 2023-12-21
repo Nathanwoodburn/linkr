e1888767f9fd70be22c932610691f38064e7523e
Nathan Woodburn <github@nathan.woodburn.au>
2023-12-21 16:14:53 +1100

    feat: Track link creation and deletion

--- render.py
+++ render.py
@@ -3,9 +3,9 @@ def links(links,host="",show_domain=False):
 
     html = ""
     for link in links:
-        html += "<div class='link'><a href='"+link[3]+"' target='_blank' class='no_display'>"+host+link[2] +" -> " + link[3]+"</a>"
+        html += "<div class='link'><a href='"+link[3]+"' target='_blank' data-umami-event='Visited link from dashboard' data-umami-event-id='"+link[2] + "' class='no_display'>"+host+link[2] +" -> " + link[3]+"</a>"
         if show_domain:
             html += "<div class='link-domain'>"+link[1]+"</div>"
-        html += "<div class='link-delete'><a class='no_display' href='/delete/"+link[2]+"'>Delete</a></div>"
+        html += "<div class='link-delete'><a class='no_display' data-umami-event='Deleted link' data-umami-event-id='"+link[2] + "' href='/delete/"+link[2]+"'>Delete</a></div>"
         html += "</div>\n"
     return html
